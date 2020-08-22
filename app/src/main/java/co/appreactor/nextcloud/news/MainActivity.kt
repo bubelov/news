71d11cd150168119f475bcf3d14a749dc7a5edd3
Igor Bubelov <igor@bubelov.com>
2020-08-22 14:17:54 +0700

    Initial commit

---
+++ app/src/main/java/co/appreactor/nextcloud/news/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.appreactor.nextcloud.news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
