4699ef96381b12d4e342e0824b2a37ea475faad3
Igor Bubelov <igor@bubelov.com>
2020-08-29 10:12:30 +0700

    Add direct login button

--- app/src/main/java/co/appreactor/nextcloud/news/AuthFragment.kt
+++ app/src/main/java/co/appreactor/nextcloud/news/AuthFragment.kt
@@ -1,14 +1,16 @@
 package co.appreactor.nextcloud.news
 
 import android.content.Intent
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.whenStarted
+import androidx.lifecycle.*
 import androidx.navigation.fragment.findNavController
 import com.nextcloud.android.sso.AccountImporter
 import com.nextcloud.android.sso.AccountImporter.IAccountAccessGranted
@@ -22,6 +24,9 @@ import kotlinx.coroutines.launch
 class AuthFragment : Fragment() {
 
     init {
+        hideStatusBarBackground()
+        invertStatusBarTextColorInLightMode()
+
         lifecycleScope.launch {
             whenStarted {
                 try {
@@ -32,7 +37,7 @@ class AuthFragment : Fragment() {
                         navigate(R.id.newsFragment)
                     }
                 } catch (e: SSOException) {
-                    login.setOnClickListener {
+                    loginViaApp.setOnClickListener {
                         it.isEnabled = false
                         showAccountPicker()
                     }
@@ -67,7 +72,7 @@ class AuthFragment : Fragment() {
 
         when (resultCode) {
             AppCompatActivity.RESULT_CANCELED -> {
-                login.isEnabled = true
+                loginViaApp.isEnabled = true
             }
 
             else -> {
@@ -88,5 +93,52 @@ class AuthFragment : Fragment() {
         } catch (e: SSOException) {
             UiExceptionManager.showDialogForException(context, e)
         }
+    }
+
+    private fun hideStatusBarBackground() {
+        lifecycle.addObserver(object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+            fun onResume() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requireActivity().window.statusBarColor = ContextCompat.getColor(
+                        requireContext(),
+                        android.R.color.transparent
+                    )
+                }
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+            fun onPause() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requireActivity().window.statusBarColor = ContextCompat.getColor(
+                        requireContext(),
+                        R.color.colorPrimaryDark
+                    )
+                }
+            }
+        })
+    }
+
+    private fun invertStatusBarTextColorInLightMode() {
+        lifecycle.addObserver(object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+            fun onResume() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    val uiMode = requireContext().resources.configuration.uiMode and
+                            Configuration.UI_MODE_NIGHT_MASK
+
+                    if (uiMode != Configuration.UI_MODE_NIGHT_YES) {
+                        view?.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                    }
+                }
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+            fun onPause() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    view?.systemUiVisibility = 0
+                }
+            }
+        })
     }
 }
