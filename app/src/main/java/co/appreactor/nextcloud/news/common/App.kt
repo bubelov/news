81e9ce421fc4fc5af9e21b452558cdaaaeaa83bc
Igor Bubelov <igor@bubelov.com>
2020-09-10 15:44:19 +0700

    Store images on external storage

--- app/src/main/java/co/appreactor/nextcloud/news/common/App.kt
+++ app/src/main/java/co/appreactor/nextcloud/news/common/App.kt
@@ -5,10 +5,13 @@ import co.appreactor.nextcloud.news.BuildConfig
 import co.appreactor.nextcloud.news.di.apiModule
 import co.appreactor.nextcloud.news.di.appModule
 import co.appreactor.nextcloud.news.logging.PersistentLogTree
+import com.squareup.picasso.OkHttp3Downloader
+import com.squareup.picasso.Picasso
 import org.koin.android.ext.android.get
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import timber.log.Timber
+import java.io.File
 
 class App : Application() {
 
@@ -25,5 +28,11 @@ class App : Application() {
         }
 
         Timber.plant(PersistentLogTree(get()))
+
+        val picasso = Picasso.Builder(get())
+            .downloader(OkHttp3Downloader(File(externalCacheDir, "images")))
+            .build()
+
+        Picasso.setSingletonInstance(picasso)
     }
 }
