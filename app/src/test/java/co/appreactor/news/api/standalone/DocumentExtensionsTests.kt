40429c7e65d579f765e2b8a7588cc21c4002cc1c
Igor Bubelov <igor@bubelov.com>
2020-10-12 01:18:46 +0700

    Add more tests

--- app/src/test/java/co/appreactor/news/api/standalone/DocumentExtensionsTests.kt
+++ app/src/test/java/co/appreactor/news/api/standalone/DocumentExtensionsTests.kt
@@ -2,6 +2,7 @@ package co.appreactor.news.api.standalone
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.w3c.dom.Document
 import java.io.File
 import javax.xml.parsers.DocumentBuilderFactory
 
@@ -9,38 +10,43 @@ class DocumentExtensionsTests {
 
     @Test
     fun getFeedType_recognizesAtom() {
-        val builder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-        val document = builder.parse(File("src/test/resources/github-curl.atom"))
-        assertEquals(FeedType.ATOM, document.getFeedType())
-    }
-
-    @Test
-    fun getFeedType_recognizesRss() {
-        val builder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-        val document = builder.parse(File("src/test/resources/ietf.rss"))
-        assertEquals(FeedType.RSS, document.getFeedType())
+        assertEquals(FeedType.ATOM, "github-curl.atom".toDocument().getFeedType())
     }
 
     @Test
     fun toAtomFeed() {
-        val builder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-
-        File("src/test/resources/github-curl.atom").apply {
-            val document = builder.parse(this)
-            val feed = document.toAtomFeed()
-            assertEquals("tag:github.com,2008:https://github.com/curl/curl/releases", feed.id)
-            assertEquals("Release notes from curl", feed.title)
-            assertEquals("https://github.com/curl/curl/releases.atom", feed.selfLink)
-            assertEquals("https://github.com/curl/curl/releases", feed.alternateLink)
+        "github-curl.atom".toDocument().toAtomFeed().apply {
+            assertEquals("tag:github.com,2008:https://github.com/curl/curl/releases", id)
+            assertEquals("Release notes from curl", title)
+            assertEquals("https://github.com/curl/curl/releases.atom", selfLink)
+            assertEquals("https://github.com/curl/curl/releases", alternateLink)
         }
 
-        File("src/test/resources/kernel.atom").apply {
-            val document = builder.parse(this)
-            val feed = document.toAtomFeed()
-            assertEquals("https://www.kernel.org/", feed.id)
-            assertEquals("The Linux Kernel Archives", feed.title)
-            assertEquals("https://www.kernel.org/feeds/all.atom.xml", feed.selfLink)
-            assertEquals("https://www.kernel.org/", feed.alternateLink)
+        "kernel.atom".toDocument().toAtomFeed().apply {
+            assertEquals("https://www.kernel.org/", id)
+            assertEquals("The Linux Kernel Archives", title)
+            assertEquals("https://www.kernel.org/feeds/all.atom.xml", selfLink)
+            assertEquals("https://www.kernel.org/", alternateLink)
         }
+    }
+
+    @Test
+    fun getFeedType_recognizesRss() {
+        assertEquals(FeedType.RSS, "ietf.rss".toDocument().getFeedType())
+    }
+
+    @Test
+    fun toRssFeed() {
+        "ietf.rss".toDocument().toRssFeed("https://foo.bar").apply {
+            assertEquals("https://tools.ietf.org/html/", id)
+            assertEquals("New RFCs", title)
+            assertEquals("https://foo.bar", selfLink)
+            assertEquals("https://tools.ietf.org/html/", alternateLink)
+        }
+    }
+
+    private fun String.toDocument(): Document {
+        val documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+        return documentBuilder.parse(File("src/test/resources/$this"))
     }
 }
