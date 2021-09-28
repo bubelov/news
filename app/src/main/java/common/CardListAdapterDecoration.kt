9f0eaed75cddb602a78b11a32afe6fe49b59a01b
Igor Bubelov <igor@bubelov.com>
2021-09-29 00:42:31 +0700

    Use even spacing for cards

--- app/src/main/java/common/CardListAdapterDecoration.kt
+++ app/src/main/java/common/CardListAdapterDecoration.kt
@@ -6,8 +6,20 @@ import androidx.recyclerview.widget.RecyclerView
 
 class CardListAdapterDecoration(private val gapInPixels: Int) : RecyclerView.ItemDecoration() {
 
-    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-        super.getItemOffsets(outRect, view, parent, state);
-        outRect.set(gapInPixels, gapInPixels, gapInPixels, gapInPixels)
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State,
+    ) {
+        val position = parent.getChildAdapterPosition(view)
+
+        val bottomGap = if (position == (parent.adapter?.itemCount ?: 0) - 1) {
+            gapInPixels
+        } else {
+            0
+        }
+
+        outRect.set(gapInPixels, gapInPixels, gapInPixels, bottomGap)
     }
 }
