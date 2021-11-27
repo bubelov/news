08822664a27595d3ee45d35e7e256b22a9dd1973
Ebrahim Byagowi <ebrahim@gnu.org>
2021-11-27 23:38:05 +0330

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("com.android.tools.build:gradle:7.0.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
         classpath("com.squareup.sqldelight:gradle-plugin:1.5.1")
