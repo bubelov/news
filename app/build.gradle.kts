9fb78d9cf43253dcf83ef1461aba3eca795d5778
Ebrahim Byagowi <ebrahim@gnu.org>
2022-04-06 12:36:31 +0430

    Use the idiomatic way to resolve Kotlin depedencies
    Gradle now injects stdlib automatically so no longer need for an explicit import, see also:
    
    https://kotlinlang.org/docs/gradle.html#dependency-on-the-standard-library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,9 +92,7 @@ gradle.projectsEvaluated {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.20")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.20")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.20")
+    implementation(kotlin("reflect"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
 
