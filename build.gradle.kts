dd2a007f39ab18334ec54c7fd351081a68facdbc
Ebrahim Byagowi <ebrahim@gnu.org>
2022-04-06 12:34:14 +0430

    Upgrade dependencies
    All the dependencies are upgraded except:
    * Koin needs more intervention about viewmodel integration.
    * Google Truth will clash espresso classes in Android tests.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.1.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.1")
         classpath("com.squareup.sqldelight:gradle-plugin:1.5.3")
     }
 }
