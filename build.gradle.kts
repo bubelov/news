e47c08c6d9fe498fe612e9717fe1f102acd2430e
Igor Bubelov <igor@bubelov.com>
2022-04-09 19:47:53 +0700

    Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.2")
+        classpath("com.android.tools.build:gradle:7.1.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.1")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
         classpath("com.squareup.sqldelight:gradle-plugin:1.5.3")
     }
 }
