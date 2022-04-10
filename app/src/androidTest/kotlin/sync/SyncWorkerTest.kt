fb9d0df537aba825aa7586a760e71553aa89749b
Igor Bubelov <igor@bubelov.com>
2022-04-10 21:43:55 +0700

    Fix Android tests

--- app/src/androidTest/kotlin/sync/SyncWorkerTest.kt
+++ app/src/androidTest/kotlin/sync/SyncWorkerTest.kt
@@ -1,30 +1,34 @@
 package sync
 
-import android.content.Context
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.work.ListenableWorker
 import androidx.work.testing.TestWorkerBuilder
-import org.junit.Assert
+import common.App
+import common.ConfRepository
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.android.ext.android.get
 import kotlin.test.assertIs
 
 @RunWith(AndroidJUnit4::class)
 class SyncWorkerTest {
 
-    private lateinit var targetContext: Context
+    private lateinit var app: App
 
     @Before
     fun setup() {
         val instrumentation = InstrumentationRegistry.getInstrumentation()
-        targetContext = instrumentation.targetContext.applicationContext
+        app = instrumentation.targetContext.applicationContext as App
     }
 
     @Test
     fun retryIfInitialSyncIncomplete() {
-        val workerBuilder = TestWorkerBuilder.from(targetContext, SyncWorker::class.java)
+        val confRepo = app.get<ConfRepository>()
+        runBlocking { confRepo.save(confRepo.get().copy(initialSyncCompleted = false)) }
+        val workerBuilder = TestWorkerBuilder.from(app, SyncWorker::class.java)
         val worker = workerBuilder.build()
         val result = worker.doWork()
         assertIs<ListenableWorker.Result.Retry>(result)
