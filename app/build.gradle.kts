ec80cdda6131acdc1a112560fbac602da3aa4561
Igor Bubelov <igor@bubelov.com>
2022-04-22 11:16:24 +0700

    Update deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,51 +92,61 @@ gradle.projectsEvaluated {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    // Kotlin extensions
+    // Simplifies non-blocking programming
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
 
-    implementation("co.appreactor:feedk:0.1.4")
-
+    // Android extensions
     implementation("androidx.core:core-ktx:1.7.0")
-    val fragmentVersion = "1.4.0"
-    implementation("androidx.fragment:fragment-ktx:$fragmentVersion")
-    debugImplementation("androidx.fragment:fragment-testing:$fragmentVersion")
-    implementation("androidx.lifecycle:lifecycle-common-java8:2.4.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+    val fragmentVer = "1.4.1"
+    implementation("androidx.fragment:fragment-ktx:$fragmentVer")
+    debugImplementation("androidx.fragment:fragment-testing:$fragmentVer")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.4.2")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.4.2")
+    val navVer = "2.4.2"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVer")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVer")
     implementation("androidx.constraintlayout:constraintlayout:2.1.3")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("androidx.browser:browser:1.4.0")
+    val workVer = "2.7.1"
+    implementation("androidx.work:work-runtime-ktx:$workVer")
+    androidTestImplementation("androidx.work:work-testing:$workVer")
 
-    val workVersion = "2.7.1"
-    implementation("androidx.work:work-runtime-ktx:$workVersion")
-    androidTestImplementation("androidx.work:work-testing:$workVersion")
+    // Feed parser
+    // Used in standalone mode
+    implementation("co.appreactor:feedk:0.1.4")
 
+    // Retrofit turns HTTP APIs into Java interfaces
+    // Used to communicate with remote backends
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    // Modern HTTP client
     implementation(platform("com.squareup.okhttp3:okhttp-bom:4.9.3"))
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:logging-interceptor")
 
-    implementation("com.squareup.sqldelight:android-driver:1.5.3")
-    implementation("com.squareup.sqldelight:coroutines-extensions:1.5.3")
+    // SQLDelight generates typesafe kotlin APIs from SQL statements
+    val sqlDelightVer = "1.5.3"
+    implementation("com.squareup.sqldelight:coroutines-extensions:$sqlDelightVer")
+    implementation("com.squareup.sqldelight:android-driver:$sqlDelightVer")
+    testImplementation("com.squareup.sqldelight:sqlite-driver:$sqlDelightVer")
 
     implementation("com.github.nextcloud:Android-SingleSignOn:0.6.1")
     implementation("com.google.android.material:material:1.7.0-alpha01")
     implementation("com.squareup.picasso:picasso:2.71828")
     implementation("io.insert-koin:koin-android:3.2.0-beta-1")
     implementation("com.jakewharton.timber:timber:5.0.1")
-    implementation("org.jsoup:jsoup:1.14.2")
+    implementation("org.jsoup:jsoup:1.14.3")
 
+    // Common testing dependencies
+    testImplementation(kotlin("test"))
     testImplementation("junit:junit:4.13.2")
     testImplementation("io.mockk:mockk:1.12.3")
-    testImplementation("com.squareup.sqldelight:sqlite-driver:1.5.1")
 
-    // Core library
+    // Core instrumented testing dependencies
+    androidTestImplementation(kotlin("test"))
     androidTestImplementation("androidx.test:core:1.4.0")
 
     // AndroidJUnitRunner and JUnit Rules
@@ -146,17 +156,15 @@ dependencies {
     // Assertions
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.ext:truth:1.4.0")
-    androidTestImplementation("com.google.truth:truth:1.0.1")
-    androidTestImplementation(kotlin("test"))
-    testImplementation(kotlin("test"))
+    androidTestImplementation("com.google.truth:truth:1.1.3")
 
     // Espresso dependencies
-    val espressoVersion = "3.4.0"
-    androidTestImplementation("androidx.test.espresso:espresso-core:$espressoVersion")
-    androidTestImplementation("androidx.test.espresso:espresso-contrib:$espressoVersion")
-    androidTestImplementation("androidx.test.espresso:espresso-intents:$espressoVersion")
-    androidTestImplementation("androidx.test.espresso:espresso-accessibility:$espressoVersion")
-    androidTestImplementation("androidx.test.espresso:espresso-web:$espressoVersion")
-    androidTestImplementation("androidx.test.espresso.idling:idling-concurrent:$espressoVersion")
-    androidTestImplementation("androidx.test.espresso:espresso-idling-resource:$espressoVersion")
+    val espressoVer = "3.4.0"
+    androidTestImplementation("androidx.test.espresso:espresso-core:$espressoVer")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:$espressoVer")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:$espressoVer")
+    androidTestImplementation("androidx.test.espresso:espresso-accessibility:$espressoVer")
+    androidTestImplementation("androidx.test.espresso:espresso-web:$espressoVer")
+    androidTestImplementation("androidx.test.espresso.idling:idling-concurrent:$espressoVer")
+    androidTestImplementation("androidx.test.espresso:espresso-idling-resource:$espressoVer")
 }
