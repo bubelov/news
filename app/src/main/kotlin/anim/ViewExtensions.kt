e0f31795de44dc1d0577a6703a67462fcbd0daea
Igor Bubelov <igor@bubelov.com>
2022-08-05 14:26:29 +0700

    Adjust animation duration

--- app/src/main/kotlin/anim/ViewExtensions.kt
+++ app/src/main/kotlin/anim/ViewExtensions.kt
@@ -3,14 +3,13 @@ package anim
 import android.view.View
 import android.view.animation.AccelerateInterpolator
 
-fun View.showSmooth() {
-    animate()
-        .alpha(1f)
-        .interpolator = AccelerateInterpolator()
-}
+fun View.showSmooth() = animateAlpha(1f)
 
-fun View.hideSmooth() {
+fun View.hideSmooth() = animateAlpha(0f)
+
+private fun View.animateAlpha(alpha: Float) {
     animate()
-        .alpha(0f)
+        .alpha(alpha)
+        .setDuration(200)
         .interpolator = AccelerateInterpolator()
 }
