3fab55256208dd26afe438971b7d3d7b8d7bc2ac
Igor Bubelov <igor@bubelov.com>
2022-10-26 18:07:45 +0700

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "co.appreactor.news"
         minSdk = 29
         targetSdk = 33
-        versionCode = 21
-        versionName = "0.4.0"
+        versionCode = 22
+        versionName = "0.4.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "news-$versionName")
     }
